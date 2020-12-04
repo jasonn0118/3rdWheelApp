30dd76ef8ebcec7445a7ec663cbeee1b5a9bd584
hy2207 <hy3980@gmail.com>
2020-12-04 01:50:35 -0800

    last version of carrental activity

--- app/src/main/java/com/example/a3rdwheel/CardetailFragment.java
+++ app/src/main/java/com/example/a3rdwheel/CardetailFragment.java
@@ -9,13 +9,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.firebase.database.DatabaseReference;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link CardetailFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class CardetailFragment extends Fragment{
-
+    //firebase
+    private DatabaseReference dbRef;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
